963e7034474a60d46aa62faad6d420a009fc1a19
charbel_hanna <charbel.hanna@inetum.com>
2025-11-06 22:24:12 +0100

    enhancing copy report

--- src/Photoshell.PowerShell/Functions/Import-Photos.ps1
+++ src/Photoshell.PowerShell/Functions/Import-Photos.ps1
@@ -1,7 +1,7 @@
 <#
     .SYNOPSIS
         Function to copy and rename photos
-        Version:1.0
+        Version:1.1
         Author: Charbel HANNA
     .DESCRIPTION
         The objective of this function is to copy photos from source to destination while performing a rename operation.
@@ -22,16 +22,58 @@ param (
     [String]$Destination,
     [Parameter(Mandatory = $False,
         HelpMessage = "specify , seperated extensions to filter")]
-    [array]$Filter = "*.JPG,*.CR2,*.CR3"
-      
+    [array]$Filter = "*.JPG,*.CR2,*.CR3",
+    [Parameter(Mandatory = $False,
+        HelpMessage = "specify the log file path")]
+    [string]$LogFilePath
 )
     
 [array]$TotalFolders
 [array]$Totalcopies
-[array]$records
+$records = [System.Collections.ArrayList]@()
 [array]$alllogs
 $properties = @{}
+$LogFileName = "log_$((Get-Date).ToString("ddMMyyyy_HHmmss")).csv"
+$copyresultsLogFileName = "CopyResults_$((Get-Date).ToString("ddMMyyyy_HHmmss")).log"
+$LogFilePath = if ($LogFilePath) { $LogFilePath } else { $PSScriptRoot }
+Function Write-Log {
+    <#
+    .DESCRIPTION 
+    Write-Log is used to write information to a log file and to the console.
     
+    .PARAMETER Severity
+    parameter specifies the severity of the log message. Values can be: Information, Warning, or Error. 
+    #>
+
+    [CmdletBinding()]
+    param(
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+        [string]$LogFileName,
+ 
+        [parameter()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Information', 'Warning', 'Error')]
+        [string]$Severity = 'Information'
+    )
+    # Write the message out to the correct channel											  
+    switch ($Severity) {
+        "Information" { Write-Host $Message -ForegroundColor Green }
+        "Warning" { Write-Host $Message -ForegroundColor Yellow }
+        "Error" { Write-Host $Message -ForegroundColor Red }
+    } 											  
+    try {
+        [PSCustomObject] [ordered] @{
+            Time     = (Get-Date -f g)
+            Message  = $Message
+            Severity = $Severity
+        } | Export-Csv -Path "$LogFilePath\$LogFileName" -Append -NoTypeInformation -Force
+    }
+    catch {
+        Write-Error "An error occurred in Write-Log() method" -ErrorAction SilentlyContinue		
+    }    
+}    
 function Import-photos {
     param (
         [string]$Inputpath,
@@ -40,17 +82,16 @@ function Import-photos {
     )
     [array]$totalFilesToCopy = @()
     [int]$Totalcopies = 0  
-    $LogFileName = "IP_$((Get-Date).ToString("ddMMyyyy_HHmmss")).log"
+   
         
-    
+    write-log "Import-Photos started processing photos from $InputPath to $BaseDestination" -LogFileName $LogFileName -Severity Information
     if (!(test-path $Source)) {
-            
-        write-host "input path is not found" -ForegroundColor Red
+        Write-log "input path $InputPath is not found" -LogFileName $LogFileName -Severity Error    
         exit 1     
     }
    
     $photos = $Filter.Split(',') | Where-Object { $_ } | ForEach-Object { Get-ChildItem $InputPath -File -Filter $_ }
-    write-host $photos.count "photos has been found" -ForegroundColor Cyan 
+    write-log "A total of $($photos.count) photos found for processing" -LogFileName $LogFileName -Severity Information
     Write-Progress -Activity "Processing Photos" -Status "Starting" -PercentComplete 0 
     $Photos | 
     ForEach-Object {
@@ -66,46 +107,48 @@ function Import-photos {
         $FullDestination = join-path $BaseDestination $BaseYear $DateTaken '\'
         $FutureFileFullName = ('{0:}{1:}_{2:}_{3:}' -f $FullDestination, $DateTaken, $CameraModel, $ImageName) 
         If (Test-Path -Path "$FullDestination") {
-            Write-Host "Folder $FullDestination Exist at $BaseDestination Skipping Folder Creation" -ForegroundColor Green
+            write-log "Folder $FullDestination Exist at $BaseDestination Skipping Folder Creation" -LogFileName $LogFileName -Severity Information
         }
         Else {
-            Write-Host "Folder $FullDestination Does not Exist at $BaseDestination" -ForegroundColor Yellow
-            Write-Host "Creating Destination Folder" $FullDestination -ForegroundColor Cyan
+            write-log "Folder $FullDestination Does not Exist at $BaseDestination" -LogFileName $LogFileName -Severity Warning
+            write-log "Creating Destination Folder $FullDestination" -LogFileName $LogFileName -Severity Information
             $createFolder = New-Item -Path $FullDestination -ItemType Directory 3>&1 
-            $TotalFolders += $CreateFolder
+            #$TotalFolders += $CreateFolder
         }
         If (!(test-path "$FutureFileFullName")) {
-            Write-Host "File:"$FutureFileFullName " was not found" -ForegroundColor Yellow   
+            write-log "File:$FutureFileFullName was not found" -LogFileName $LogFileName -Severity Information
             $copy = Copy-Item -path $_.FullName -Destination $FutureFileFullName -verbose 3>&1  -passthru
             $Totalcopies++
-            $Totalfilestocopy += $copy
+            $status = "Copied"
+            #$log = Write-Output $copy -Verbose 2>&1
+            #$Totalfilestocopy += $copy
                 
         }
         Else {
-            $Log = Write-Output "File:"$FutureFileFullName " was found at $FullDestination | skippping copy" -Verbose 2>&1
+            write-log  "File:$FutureFileFullName  was found at $FullDestination | skippping copy" -LogFileName $LogFileName -Severity Warning
+            #$Log = Write-Output "File:$FutureFileFullName  was found at $FullDestination | skippping copy" -Verbose 2>&1
             $Status = "Skipped"
-            $alllogs += $log
+            #$alllogs += $log
         }
         
-        $collection = @{
-            'ImageName'         = $ImageName
-            'CopyStatus'        = $Status
-            'DestinationFolder' = $BaseDestination
-            'FileName'          = $FutureFileFullName
+        $collection = [ordered] @{
+            ImageName         = $ImageName
+            CopyStatus        = $Status
+            Destination       = $BaseDestination
+            FileName          = $FutureFileFullName
         } 
-        foreach ($keys in $collection.keys) {
-            $properties[$keys] = $collection[$keys]
-        }
-        $Items = [PSCustomObject]$properties
-        $records += $Items
+       
+        $Items = [PSCustomObject]$collection
+        $records.Add($Items) | Out-Null
     }
     Write-Progress -Activity "Processing Photos" -Status "Completed" -PercentComplete 100  
-    write-host $Totalcopies "photo(s) were copied"
+    write-log "Import-Photos completed processing photos from $InputPath to $BaseDestination" -LogFileName $LogFileName -Severity Information
+    write-log "$Totalcopies photo(s) were copied" -LogFileName $LogFileName -Severity Information
     $TotalFolders | Out-File .\$LogFileName 
     #$Totalfilestocopy | out-file .\$LogFileName
-    $alllogs | Out-File .\$LogFileName
-    $records | Out-File .\$logFileName
-    write-host "copy logs are stored in" (Get-ChildItem -path .\$LogFileName).FullName 
+    $alllogs | Out-File .\$copyresultsLogFileName
+    $records | Out-File .\$copyresultsLogFileName
+    write-host "copy report are stored in" (Get-ChildItem -path $LogFilePath\$copyresultsLogFileName).FullName 
 }
 # main script
 try {
